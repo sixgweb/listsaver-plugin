0a030f4b2e3375c8b28c2f6f1057a06c29576106
Ryan Showers <ryanshowers@6gwebdesign.com>
2023-09-10 07:09:48 -0600

    Declare missing options var

--- models/Settings.php
+++ models/Settings.php
@@ -14,6 +14,7 @@ class Settings extends \System\Models\SettingModel
 
     public function getPathOptions()
     {
+        $options = [];
         $custom = [];
 
         if ($this->enabled_paths) {
